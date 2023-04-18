54f82e0a7ba27d4204aa81002f1c010c7ff25dfb
Li <muhanli@ead.ubc.ca>
2023-04-18 12:02:48 -0700

    changed expire time for signed url

--- cdk/lambdas/getSignedURL/getSignedURL.js
+++ cdk/lambdas/getSignedURL/getSignedURL.js
@@ -17,6 +17,7 @@ exports.handler = async(event) => {
         const signedURL = S3Client.getSignedURL('getObject', {
             Bucket: Bucket,
             Key: Key,
+            Expires: 300
         })
         
         return {
