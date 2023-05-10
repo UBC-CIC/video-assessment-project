6723de546d01bcb2e2ea409f70bb98cbbdf43c7c
Li <muhanli@ead.ubc.ca>
2023-05-09 17:00:40 -0700

    updated keys in dynamodb

--- cdk/getblurredrecordingstack.py
+++ cdk/getblurredrecordingstack.py
@@ -40,7 +40,7 @@ class RecordWithFaceBlurStack(cdk.Stack):
         ## Allocate dynamodb table to manage user info regarding recordings
         videodata = dynamodb.Table(self, "videodata",
             partition_key=dynamodb.Attribute(name="userid", type=dynamodb.AttributeType.STRING),
-            sort_key=dynamodb.Attribute(name="assessmentid", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="starttime", type=dynamodb.AttributeType.STRING)
         )
 
         ###############################################################################################
